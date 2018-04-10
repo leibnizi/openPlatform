c659de37e184268f0934e97bdbb8f1ab388b3db9
Carmelo <carmelo@CarmelodeMacBook-Pro.local>
2018-04-10 17:54:24 +0800

    add ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import reducer from './reducers/index';
+import { createStore } from 'redux';
 import * as React from 'react';
 import './App.css';
+import './styles/App.less';
+
+export const store = createStore(
+  reducer
+);
 
 const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="app">
+            <header className="header" />
+            <section className="section">
+              <section className="logo">
+                <img src={logo} alt="头部logo" />
+                <p>商家后台管理系统</p>
+              </section>
+              <section className="navigation">
+                <div>Navigation</div>
+              </section>
+            </section>
+            <section className="body">
+              <div>body</div>
+            </section>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }

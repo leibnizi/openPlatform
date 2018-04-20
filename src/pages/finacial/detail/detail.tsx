66b42be62ef444553e10e3186affce70a2d19764
wedge <wenmjie.jiang@msparis.com>
2018-04-20 18:50:35 +0800

    fincial

--- src/pages/finacial/detail/detail.tsx
+++ src/pages/finacial/detail/detail.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
+import { connect } from 'react-redux'
 import { TimePicker, Table } from 'antd'
-// import { fetchGet } from '../../../services/httpRequest'
+import './detail.less'
 
-export default class Detail extends React.Component<any, any> {
+class Detail extends React.Component<any, any> {
 
   constructor(props: any) {
     super(props)
-
     this.state = {
       formNum: '',
       startTime: '',
@@ -21,24 +21,23 @@ export default class Detail extends React.Component<any, any> {
   }
 
   handleSubmit = (e: any) => {
-    const { startTime, endTime, formNum } = this.state
-    let tableData:any[] = []
-    // const body = {begin:string,end:string}
     e.preventDefault()
+    const { startTime, endTime, formNum } = this.state
+    const token = this.props.state.userInfo.token
+    let tableData: any[] = []
 
-    fetch(`/api/financial/info_list?begin=${startTime}&?end=${endTime}&id=${Number(formNum)}`)
-      .then(res=>res.json())
-      .then((Detail:any) => {
-        console.log('Detail.data', Detail, Detail.data)
-        Detail.data.map((item:any, index:number)=>
+    fetch(`/api/financial/info_list?token=${token}&begin=${startTime}&end=${endTime}&id=${Number(formNum)}`)
+      .then(res => res.json())
+      .then((Detail: any) => {
+        Detail.data.map((item: any, index: number) =>
           tableData.push({
-            amount:item.amount,
-            month:item.month,
+            amount: item.amount,
+            month: item.month,
             supplier_id: item.supplier_id,
             year: item.year
           })
         )
-        this.setState({tableData})
+        this.setState({ tableData })
       })
   }
 
@@ -76,18 +75,18 @@ export default class Detail extends React.Component<any, any> {
         title: '操作',
         dataIndex: 'moshi'
       }
-    ];
+    ]
 
     return (
-      <div>
-        <p>对账明细</p>
+      <div className='detail'>
+        <p className='detailHead'>对账明细</p>
         <p>
           下单时间：
           <TimePicker onChange={(e) => this.startTime(e)} />
           -
           <TimePicker onChange={(e) => this.endTime(e)} />
         </p>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
           <label>
             账单编号:
             <input type="text" value={formNum} onChange={(e) => this.handleChange(e.target.value)} />
@@ -95,8 +94,23 @@ export default class Detail extends React.Component<any, any> {
           <input type="submit" value="查询" />
           <p>如对账单有疑问，请联系对接人</p>
         </form>
-        <Table className='producttab' columns={columns} dataSource={tableData} bordered={true} />
+        <Table
+          className='producttable'
+          columns={columns}
+          dataSource={tableData}
+          bordered={true}
+        />
       </div>
     )
   }
 }
+
+const mapStateToProps: any = (state: object) => ({
+  state: state
+})
+
+const mapDispatchToProps: any = (dispatch: any) => ({
+  dispatch,
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail)

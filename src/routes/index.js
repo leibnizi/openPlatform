f5e0ea5a4365fc3556ab4e0344dd2b26bdeb1aa8
wedge <wenmjie.jiang@msparis.com>
2018-04-23 18:06:42 +0800

    delete infos

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import Product from './operation'
 import Business from './business'
 import Fincial from './fincial'
 import HelpMessage from './helpMessage'
-import Infos from '../pages/business/infos'
 
 export default [
   {

84b15f65473b81ee159d3b7261fbc87253530beb
wedge <wenmjie.jiang@msparis.com>
2018-04-23 11:14:59 +0800

    help

--- src/pages/helpMessage/announcement/announcement.tsx
+++ src/pages/helpMessage/announcement/announcement.tsx
@@ -1,11 +1,42 @@
-import * as React from 'react';
+import * as React from 'react'
+import { connect } from 'react-redux'
+import { httpGet } from '../../../services/httpRequest'
+// import { TimePicker, Table, Button } from 'antd'
+import './announcement.less'
 
-export default class Announcement extends React.Component {
+class Announcement extends React.Component<any, any> {
+
+  constructor(props: any) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    // fetch('/api/financial/info_list').then(Detail=>console.log('Detail',Detail))
+    const token = this.props.state.userInfo.token
+    httpGet(`/api/financial/info_list?token=${token}`)
+      .then(res=>console.log(res))
+  }
+
   render() {
+
     return (
-      <div>
-        Announcement
+      <div className='announcement'>
+        <p className='helpHead'>商家公告</p>
+
       </div>
     )
   }
 }
+
+const mapStateToProps: any = (state: object) => ({
+  state: state
+})
+
+const mapDispatchToProps: any = (dispatch: any) => ({
+  dispatch,
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Announcement)
--- src/pages/helpMessage/index.tsx
+++ src/pages/helpMessage/index.tsx
@@ -1,11 +1,42 @@
-import * as React from 'react';
+import * as React from 'react'
+import { connect } from 'react-redux'
+import { httpGet } from '../../services/httpRequest'
+// import { TimePicker, Table, Button } from 'antd'
+import './index.less'
 
-export default class Customerservice extends React.Component {
+class Customerservice extends React.Component<any, any> {
+
+  constructor(props: any) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    // fetch('/api/financial/info_list').then(Detail=>console.log('Detail',Detail))
+    const token = this.props.state.userInfo.token
+    httpGet(`/api/financial/info_list?token=${token}`)
+      .then(res=>console.log(res))
+  }
+
   render() {
+
     return (
-      <div>
-        Customerservice
+      <div className='customer'>
+        <p className='customerHead'>联系客服</p>
+
       </div>
     )
   }
 }
+
+const mapStateToProps: any = (state: object) => ({
+  state: state
+})
+
+const mapDispatchToProps: any = (dispatch: any) => ({
+  dispatch,
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Customerservice)

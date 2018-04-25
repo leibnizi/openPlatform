19510af4235a22a0000cdda6e619452e999152ed
wedge <wenmjie.jiang@msparis.com>
2018-04-25 16:39:59 +0800

    detail

--- src/pages/finacial/detail/detail.tsx
+++ src/pages/finacial/detail/detail.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 import { TimePicker, Table, Button } from 'antd'
+import { httpGet } from '../../../services/httpRequest'
 import './detail.less'
 
 class Detail extends React.Component<any, any> {
@@ -16,29 +17,33 @@ class Detail extends React.Component<any, any> {
     }
   }
 
-  componentDidMount() {
-    // fetch('/api/financial/info_list').then(Detail=>console.log('Detail',Detail))
-  }
-
-  handleSubmit = (e: any) => {
-    e.preventDefault()
+  getData = () => {
     const { startTime, endTime, formNum } = this.state
     const token = this.props.state.userInfo.token
     let tableData: any[] = []
 
-    fetch(`/api/financial/info_list?token=${token}&begin=${startTime}&end=${endTime}&id=${Number(formNum)}`)
-      .then(res => res.json())
+    httpGet(`/api/financial/info_list?token=${token}&begin=${startTime}&end=${endTime}&id=${formNum}`)
       .then((Detail: any) => {
-        Detail.data.map((item: any, index: number) =>
+        Detail.data.data.map((item: any, index: number) =>
           tableData.push({
             amount: item.amount,
             month: item.month,
             supplier_id: item.supplier_id,
-            year: item.year
+            year: item.year,
+            key: index
           })
         )
         this.setState({ tableData })
       })
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  handleSubmit = (e: any) => {
+    e.preventDefault()
+    this.getData()
   }
 
   handleChange = (e: string) => {
@@ -61,54 +66,58 @@ class Detail extends React.Component<any, any> {
         title: '账单编号',
         dataIndex: 'supplier_id',
         key: 'supplier_id',
-        render: (text: string) => <a href="#">{text}</a>
+        align: 'center',
       }, {
         title: '账单时间',
         className: 'column-money',
         dataIndex: 'year',
-        key: 'year'
+        key: 'year',
+        align: 'center',
       }, {
         title: '可提现金额',
         dataIndex: 'amount',
-        key: 'amount'
+        key: 'amount',
+        align: 'center',
       }, {
         title: '账单状态',
         dataIndex: 'month',
         key: 'month',
+        align: 'center',
       }, {
         title: '操作',
         dataIndex: 'moshi',
         key: 'moshi',
+        align: 'center',
       }
     ]
 
     return (
       <div className='detail'>
         <p className='detailHead'>对账明细</p>
-        
+
         <form onSubmit={(e) => this.handleSubmit(e)}>
           <label className='formTime'>
             <span className='timeFont'>下单时间：</span>
-            <TimePicker 
+            <TimePicker
               className='timePick'
-              onChange={(e) => this.startTime(e)} 
+              onChange={(e) => this.startTime(e)}
             />
             <span className='timeSymbol'>-</span>
-            <TimePicker 
+            <TimePicker
               className='timePick'
-              onChange={(e) => this.endTime(e)} 
+              onChange={(e) => this.endTime(e)}
             />
           </label>
           <label className='formsecond'>
             <span>账单编号:</span>
-            <input 
-              type="text" 
-              value={formNum} 
-              onChange={(e) => this.handleChange(e.target.value)} 
+            <input
+              type="text"
+              value={formNum}
+              onChange={(e) => this.handleChange(e.target.value)}
             />
           </label>
           <div className='querySubmit'>
-            <Button 
+            <Button
               className='querySubmitButton'
               type="primary"
               htmlType="submit"

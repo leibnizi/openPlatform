55e50aea60edbb97e0a9c529c4a09c8e4e71c8fa
wedge <wenmjie.jiang@msparis.com>
2018-04-28 14:08:33 +0800

    request

--- src/services/httpRequest.ts
+++ src/services/httpRequest.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosInstance, AxiosPromise} from 'axios';
+import axios, { AxiosInstance, AxiosPromise } from 'axios';
 import * as Cookies from 'js-cookie';
 import { message, Modal } from 'antd';
 
@@ -11,26 +11,27 @@ var is_modal_show = true;
  */
 
 function checkStatus(res: any) {
-    if(res.status == 200){
-        if (res.data.status_code == 0) {
-            return res
-        }else {
-            if((res.data.status_code == 210 || res.data.status_code == 202) && is_message_show){
-                warning(res.data.msg );
-                is_message_show = false;
-                return false;
-            }else if(is_message_show){
-                console.log(res);
-                message.error(res.data.msg || '失败',1);
-                is_message_show = false;
-            }
-            return res
-        }
-    }else {
-        error();
+  if (res.status == 200) {
+    if (res.data.status_code == 0) {
+      return res
+    } else {
+      if ((res.data.status_code == 210 || res.data.status_code == 202) && is_message_show) {
+        warning(res.data.msg);
         is_message_show = false;
         return false;
+      } else if (is_message_show) {
+        console.log(res);
+        message.error(res.data.msg || '失败', 1);
+        is_message_show = false;
+      }
+      return res
     }
+  } else {
+    
+    error();
+    is_message_show = false;
+    return false;
+  }
 }
 
 /**
@@ -39,96 +40,96 @@ function checkStatus(res: any) {
  * @return {any}
  */
 function handelData(res: any) {
-    const data = res.data
-    if (res.status_code != 0) {
+  const data = res.data
+  if (res.status_code != 0) {
     //     if (data.status_code == '11008') {
     // // if (res.statusText !== 'ok') {
     // //     if (data.status_code === '11008') {
-          
+
     //     }
     //     else {
     //         return data
     //     }
-      // message.error(res.msg)
-      return data
-    }
-    else {
-        return data
-    }
+    // message.error(res.msg)
+    return data
+  }
+  else {
+    return data
+  }
 }
 
 function handleError(error: any) {
-    return {success: false}
+  return { success: false }
 }
 
 /**
  * 警告弹窗
  */
 function warning(msg) {
-    if(is_modal_show){
-        Modal.warning({
-            title: '警告',
-            content: msg,
-            okText:'确定',
-            onOk() {
-                window.location.href =  window.location.origin+ "/login";
-                is_modal_show = true;
-                Cookies.remove('token')
-            },
-        });
-    }
+  if (is_modal_show) {
+    Modal.warning({
+      title: '警告',
+      content: msg,
+      okText: '确定',
+      onOk() {
+        window.location.href = window.location.origin + "/login";
+        is_modal_show = true;
+        Cookies.remove('name')
+      },
+    });
+  }
 }
 
 /**
  * 网络错误
  */
 function error() {
-    if(is_modal_show){
-        Modal.error({
-            title: '错误',
-            content: '网络错误返回登录，请联系系统人员',
-            okText:'确定',
-            onOk() {
-                window.location.href =  window.location.origin+ "/login";
-                is_modal_show = true;
-            },
-        });
-    }
+  if (is_modal_show) {
+    Modal.error({
+      title: '错误',
+      content: '网络错误返回登录，请联系系统人员',
+      okText: '确定',
+      onOk() {
+        window.location.href = window.location.origin + "/login";
+        is_modal_show = true;
+      },
+    });
+  }
 }
 
 /**
  * 创建axios
  */
 const instance = axios.create({
-    baseURL: "http://open-erp.test.msparis.com",
-    headers: {
-       // withCredentials: false
-    },
-    params: {},
-    data: {},
-    timeout: 50000
+  baseURL: "http://open-erp.test.msparis.com",
+  headers: {
+    // withCredentials: false
+  },
+  params: {},
+  data: {},
+  timeout: 50000
 });
 
 
 const enhanceAxiosInstance = (instance: AxiosInstance) => {
-    // let token = {
-    //     token: '19$$b5fbab2e48ad5a0470ef8a351f9b6aa9'
-    // } ;
-    //let token = {}
-    
-    instance.interceptors.request.use(function (config:any) {
-        let token = Cookies.getJSON('token');
-        config.params['token'] = token;
-        config.data['token'] = token;
-        console.log(config, 1212);
-        console.log(instance.defaults.params,123);
-        return config;
-    });
+  // let token = {
+  //     token: '19$$b5fbab2e48ad5a0470ef8a351f9b6aa9'
+  // } ;
+  //let token = {}
 
-    instance.interceptors.response.use(checkStatus);
-    instance.interceptors.response.use(handelData);
-    //instance.interceptors.response.use(handleError);
-    return instance
+  instance.interceptors.request.use(function (config: any) {
+    let token = Cookies.getJSON('token');
+    config.params['token'] = token;
+    config.data['token'] = token;
+    console.log(config, 1212);
+    console.log(instance.defaults.params, 123);
+    return config;
+  });
+
+  instance.interceptors.response.use(checkStatus);
+  instance.interceptors.response.use(handelData);
+  //instance.interceptors.response.use(handleError);
+  return instance
 }
 
 //发送请求的方法

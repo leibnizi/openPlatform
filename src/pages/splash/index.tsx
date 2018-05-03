c5c28adaaeee28112e2c5ecf3fd215b02b682d8a
wedge <wenmjie.jiang@msparis.com>
2018-05-03 14:18:13 +0800

    ..

--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -16,83 +16,26 @@ export default class Splash extends React.Component<any, any> {
     }
   }
 
-  // componentDidMount() {
-  //   fetchUtil('api/login', { name: '12', mobile: '1221' }).then(v => console.log('login', v))
-  // }
-
-  // handleChangeId = (e: string) => {
-  //   this.setState({ id: e })
-  // }
-
-  // handleChangePass = (e: string) => {
-  //   this.setState({ password: e })
-  // }
-
-  // handleSubmit = (e: any) => {
-  //   e.preventDefault()
-  //   const { id, password } = this.state
-  //   fetchUtil('api/login', { name: String(id), mobile: String(password) })
-  //     .then(v => console.log('login', v))
-  // }
-
-  // handleOk = () => {
-  //   const { id, password } = this.state
-  //   fetchUtil('api/login', { name: String(id), mobile: String(password) })
-  //     .then(v => console.log('login', v))
-  // }
-
-  // modaloff = () => {
-  //   this.setState({ modalshow: false })
-  // }
-
-  // modalon = () => {
-  //   this.setState({ modalshow: true })
-  // }
-
   render() {
     // const { modalshow, loading, id, password } = this.state
     return (
       <div className='splash'>
         <p className='name splashchild'>商家后台管理系统</p>
-        <Link
-          to='/login'
-          className='login splashchild'
-        >
-          登录
+        <div className='splashmid'>
+          <Link
+            to='/login'
+            className='login'
+          >
+            登录
         </Link>
-        <Link
-          to='/register'
-          className='register splashchild'
-        >
-          申请加入女神派
+          <Link
+            to='/register'
+            className='register'
+          >
+            申请加入女神派
         </Link>
-        {/* <Modal
-          visible={modalshow}
-          title=""
-          
-          onOk={() => this.modaloff()}
-          onCancel={() => this.setState({ modalshow: false })}
-          footer={[
-            <Button 
-              className='loginButton' 
-              key="submit" 
-              type="primary" 
-              loading={loading} 
-              onClick={() => this.handleOk()}
-            >
-              登录
-            </Button>,
-          ]}
-        >
-          <label>
-            账户:
-            <input type="text" value={id} onChange={(e) => this.handleChangeId(e.target.value)} />
-          </label>
-          <label>
-            密码:
-          <input type="text" value={password} onChange={(e) => this.handleChangePass(e.target.value)} />
-          </label>
-        </Modal> */}
+        </div>
+
       </div>
     )
   }

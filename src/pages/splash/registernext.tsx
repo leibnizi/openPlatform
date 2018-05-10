8599f76e89e89f4b3386cb991e8bcb7cc0fe303d
wedge <wenmjie.jiang@msparis.com>
2018-05-10 14:44:40 +0800

    ..

--- src/pages/splash/registernext.tsx
+++ src/pages/splash/registernext.tsx
@@ -152,8 +152,6 @@ class RegisterNext extends React.Component<any, any> {
           .then((res: any) => {
             if (res.status_code === 0) {
               this.props.gotoStep(e, 2)
-            } else {
-              message.error(res.msg)
             }
           })
       }

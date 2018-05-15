e77bdd8be02b15c978f4c0368c215e644a1bfd8b
wedge <wenmjie.jiang@msparis.com>
2018-05-15 14:45:03 +0800

    modify

--- src/redux/reducers/business/bill.ts
+++ src/redux/reducers/business/bill.ts
@@ -1,39 +1,31 @@
 interface BillInfoState {
-  bank:string,
-  account:string,
-  payee:string,
-  finance_state:string,
+  bank: string,
+  account: string,
+  payee: string,
+  finance_state: string,
   isEdit: boolean
 }
 
-const initBillState: BillInfoState  = {
-  bank:'',
-  account:'',
-  payee:'',
-  finance_state:'',
+const initBillState: BillInfoState = {
+  bank: '',
+  account: '',
+  payee: '',
+  finance_state: '',
   isEdit: true
 }
 
 const billInfos = (state: BillInfoState = initBillState, action: any) => {
   switch (action.type) {
     case 'GET_BILL_SUCCESS':
-      const newState = Object.assign({}, state, action.data, {isEdit:false});
+      const newState = Object.assign({}, state, action.data, { isEdit: false });
       return newState;
-    default:
-      return state
-  }
-}
-
-const postbillInfos = (state:any = {}, action: any) => {
-  switch (action.type) {
     case 'POST_BILL_INFO_SUCCESS':
-      const newState = Object.assign({}, state, action.data);
-      return newState;
+      return Object.assign({}, state, action.data)
     default:
       return state
   }
 }
 
-export default { 
-  billInfos, postbillInfos 
+export default {
+  billInfos
 }
